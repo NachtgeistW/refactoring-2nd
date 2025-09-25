7c8cbe074a2b45b6a5ced6fcaf532f26703cad1f
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2025-09-25 16:38:25 +0800

    改回去

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -31,25 +31,20 @@ const defaultOptions: Options = {
   },
   sortFn: (a, b) => {
     // Sort order: folders first, then files. Sort folders and files alphabeticall
-    // if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
-    //   // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
-    //   // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
-    //   return a.displayName.localeCompare(b.displayName, undefined, {
-    //     numeric: true,
-    //     sensitivity: "base",
-    //   })
-    // }
+    if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+      // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+      // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+      return a.displayName.localeCompare(b.displayName, undefined, {
+        numeric: true,
+        sensitivity: "base",
+      })
+    }
 
-    // if (!a.isFolder && b.isFolder) {
-    //   return 1
-    // } else {
-    //   return -1
-    // }
-
-    return a.displayName.localeCompare(b.displayName, undefined, {
-      numeric: true,
-      sensitivity: "base",
-    })
+    if (!a.isFolder && b.isFolder) {
+      return 1
+    } else {
+      return -1
+    }
   },
   filterFn: (node) => node.slugSegment !== "tags",
   order: ["filter", "map", "sort"],
